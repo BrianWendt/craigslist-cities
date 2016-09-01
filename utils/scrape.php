de38f3676a2ca65c0d51a1e6559d895cfdd44880
Brian Wendt <brian.m.wendt@gmail.com>
2016-08-31 18:57:05 -0700

    Removing caching used during testing

--- utils/scrape.php
+++ utils/scrape.php
@@ -4,13 +4,8 @@ include '../vendor/autoload.php';
 
 use Masterminds\HTML5;
 
-if (true) {
-    $html = file_get_contents('cache.html');
-} else {
-    $url = 'https://www.craigslist.org/about/sites';
-    $html = file_get_contents($url);
-    file_put_contents('cache.html', $html);
-}
+$url = 'https://www.craigslist.org/about/sites';
+$html = file_get_contents($url);
 
 $data = [];
 
@@ -23,16 +18,16 @@ $section = $dom->getElementsByTagName('section')->item(0);
 $region_names_list = $section->getElementsByTagName('h1');
 $region_list = $finder->query("//*[contains(@class, 'colmask')]", $section);
 
-foreach($region_list as $region_i => $region){
+foreach ($region_list as $region_i => $region) {
     $region_name = $region_names_list->item($region_i)->textContent;
     $data[$region_name] = [];
     $state_list = $region->getElementsByTagName('h4');
     $city_lists = $region->getElementsByTagName('ul');
-    foreach($state_list as $state_i => $state){
+    foreach ($state_list as $state_i => $state) {
         $state_name = $state->textContent;
         $data[$region_name][$state_name] = [];
         $city_list = $city_lists->item($state_i)->getElementsByTagName('li');
-        foreach($city_list as $city_i => $city){
+        foreach ($city_list as $city_i => $city) {
             $a = $city->getElementsByTagName('a')->item(0);
             $link = $a->getAttribute('href');
             $sub_domain = substr($link, 2, strpos($link, '.') - 2);
